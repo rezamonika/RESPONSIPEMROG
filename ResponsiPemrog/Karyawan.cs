c8e913232e0134d88473cab56ff2475ced66ed4a
rezamonika <48460821+rezamonika@users.noreply.github.com>
2019-05-02 09:11:08 +0700

    Update Karyawan.cs

--- ResponsiPemrog/Karyawan.cs
+++ ResponsiPemrog/Karyawan.cs
@@ -1,33 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ResponsiPemrog2366
+namespace Responsi
 {
-    public class Karyawan
+    class Karyawan
     {
-        public Karyawan()
-        {
-        }
-
-        public Karyawan(string nik, string nama, int gajibulanan)
-        {
-            if (GajiBulanan < 0)
-            {
-                Console.WriteLine("Gaji harus diatas 0!");
-            }
-            else
-            {
-                Nik = nik;
-                Nama = nama;
-                GajiBulanan = gajibulanan;
-            }
-        }
-
         public string Nik { get; set; }
         public string Nama { get; set; }
         public int GajiBulanan { get; set; }
+
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+
+            GajiBulanan = gajibulanan < 0 ? 0 : gajibulanan;
+        }
     }
 }
